98c3626678b75acaa9371ad7235d6e23df269b50
郭佳 <guo_jia_jia@hotmail.com>
2017-11-22 16:38:02 +0800

    添加配置文件

--- SyncService/Service1.cs
+++ SyncService/Service1.cs
@@ -19,6 +19,31 @@ namespace SyncService
 
         protected override void OnStart(string[] args)
         {
+
+            var num = Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["timeinterval"]);
+
+            System.Threading.Timer timer = new System.Threading.Timer(Timer1_Elapsed, null, 0, num);
+        }
+
+        private void Timer1_Elapsed(object o)
+        {
+
+            //linux 下 mono 运行windows服务 命令:
+            //mono-service -l:/var/run/myserver.lock /usr/local/myserver/myserver.exe
+
+            try
+            {
+                //TeacherDal.CheckAbnormalClassEndTimer();
+
+                //TeacherDal.CheckAbnormalCoursePicTimer();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
         }
 
         protected override void OnStop()
