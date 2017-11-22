1f405b4aceb9f294d4280e5aedd2f7f2071dee1d
郭佳 <guo_jia_jia@hotmail.com>
2017-11-22 16:23:45 +0800

    seo

--- SyncBll/SyncDo.cs
+++ SyncBll/SyncDo.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace SyncBll
 {
     public class SyncDo
     {
 
-        public int GetOrgNewInfo()
+        public decimal GetOrgNewInfo(int ogrId)
         {
-            return 1;
+            //`HistoryID`, `OrgID`, `ActionType`, `DisCount`, `OrgMoney`, `OrgGift`, `OrgValue`, `AfterValue`, `BookSetID`, `StudentID`, `SchoolID`, `SchoolName`, `OperationUser`, `Remarks`, `MfgRemarks`, `CreateTime`
+
+            string sql = "SELECT   IFNULL(SUM(OrgMoney),0) FROM `mfg_org_transaction` WHERE orgid=@OrgId LIMIT 0, 1 ";
+          object obj=DBHelper.GetScalarFile(sql,new MySqlParameter("@OrgId", ogrId));
+
+            decimal leftMoney = Convert.ToDecimal(obj);
+
+            return leftMoney;
+
+
 
         }
 
