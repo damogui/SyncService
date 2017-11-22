0c2e07ac0db624f7cf7bf71c924596166d840f80
郭佳 <guo_jia_jia@hotmail.com>
2017-11-22 16:34:59 +0800

    添加代码

--- SyncBll/DBHelper.cs
+++ SyncBll/DBHelper.cs
@@ -12,7 +12,7 @@ namespace SyncBll
     {
         //数据库连接字符串(web.config来配置)，可以动态更改ConnectionString支持多数据库.		
         public static string ConnectionString = ConfigurationManager.ConnectionStrings["wordConn"].ConnectionString;
-   
+
         #region 原生
         /// <summary>
         /// 执行SQL语句，返回影响的记录数，没有传入事务
@@ -283,7 +283,7 @@ namespace SyncBll
             }
         }
 
-    
+
 
         #region 获取单条记录
         /// <summary>
@@ -316,7 +316,7 @@ namespace SyncBll
         #region SetCommand、Adapter
 
 
-     
+
         #endregion
         /// <summary>
         /// 获取单个实体
@@ -483,5 +483,84 @@ namespace SyncBll
 
             }
         }
+
+
+
+        #region 事务
+        /// <summary>
+          /// 用指定的数据库连接字符串执行一个命令并返回一个数据集的第一列
+          /// </summary>
+          /// <remarks>
+          ///例如:
+          /// Object obj = ExecuteScalar(connString, CommandType.StoredProcedure, "PublishOrders", new MySqlParameter("@prodid", 24));
+          /// </remarks>
+          ///<param name="connectionString">一个有效的连接字符串</param>
+          /// <param name="cmdType">命令类型(存储过程, 文本, 等等)</param>
+          /// <param name="cmdText">存储过程名称或者sql命令语句</param>
+          /// <param name="commandParameters">执行命令所用参数的集合</param>
+          /// <returns>用 Convert.To{Type}把类型转换为想要的 </returns>
+        public static object ExecuteScalar(string connectionString, CommandType cmdType, string cmdText, params MySqlParameter[] commandParameters)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                PrepareCommand(cmd, connection, null, cmdType, cmdText, commandParameters);
+                object val = cmd.ExecuteScalar();
+                cmd.Parameters.Clear();
+                return val;
+            }
+        }
+
+        /// <summary>
+          /// 准备执行一个命令
+          /// </summary>
+          /// <param name="cmd">sql命令</param>
+          /// <param name="conn">OleDb连接</param>
+          /// <param name="trans">OleDb事务</param>
+          /// <param name="cmdType">命令类型例如 存储过程或者文本</param>
+          /// <param name="cmdText">命令文本,例如:Select * from Products</param>
+          /// <param name="cmdParms">执行命令的参数</param>
+        private static void PrepareCommand(MySqlCommand cmd, MySqlConnection conn, MySqlTransaction trans, CommandType cmdType, string cmdText, MySqlParameter[] cmdParms)
+        {
+            if (conn.State != ConnectionState.Open)
+                conn.Open();
+            cmd.Connection = conn;
+            cmd.CommandText = cmdText;
+            if (trans != null)
+                cmd.Transaction = trans;
+            cmd.CommandType = cmdType;
+            if (cmdParms != null)
+            {
+                foreach (MySqlParameter parm in cmdParms)
+                    cmd.Parameters.Add(parm);
+            }
+        }
+
+
+        /// <summary>
+          ///使用现有的SQL事务执行一个sql命令（不返回数据集）
+          /// </summary>
+          /// <remarks>
+          ///举例:
+          /// int result = ExecuteNonQuery(connString, CommandType.StoredProcedure, "PublishOrders", new MySqlParameter("@prodid", 24));
+          /// </remarks>
+          /// <param name="trans">一个现有的事务</param>
+          /// <param name="cmdType">命令类型(存储过程, 文本, 等等)</param>
+          /// <param name="cmdText">存储过程名称或者sql命令语句</param>
+          /// <param name="commandParameters">执行命令所用参数的集合</param>
+          /// <returns>执行命令所影响的行数</returns>
+        public static int ExecuteNonQuery(MySqlTransaction trans, CommandType cmdType, string cmdText, params MySqlParameter[] commandParameters)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            PrepareCommand(cmd, trans.Connection, trans, cmdType, cmdText, commandParameters);
+            int val = cmd.ExecuteNonQuery();
+            cmd.Parameters.Clear();
+            return val;
+        }
+
+        #endregion
+
+
+
     }
 }
--- SyncBll/SyncDo.cs
+++ SyncBll/SyncDo.cs
@@ -16,15 +16,43 @@ namespace SyncBll
             //`HistoryID`, `OrgID`, `ActionType`, `DisCount`, `OrgMoney`, `OrgGift`, `OrgValue`, `AfterValue`, `BookSetID`, `StudentID`, `SchoolID`, `SchoolName`, `OperationUser`, `Remarks`, `MfgRemarks`, `CreateTime`
 
             string sql = "SELECT   IFNULL(SUM(OrgMoney),0) FROM `mfg_org_transaction` WHERE orgid=@OrgId LIMIT 0, 1 ";
-          object obj=DBHelper.GetScalarFile(sql,new MySqlParameter("@OrgId", ogrId));
+            object obj = DBHelper.GetScalarFile(sql, new MySqlParameter("@OrgId", ogrId));
 
             decimal leftMoney = Convert.ToDecimal(obj);
 
             return leftMoney;
 
-
+            #region 事务
+            // MySqlConnection conn =
+            //             new MySqlConnection(connectionString);
+            //            conn.Open();
+            //            MySqlTransaction tran = conn.BeginTransaction();
+            //            object obj = MySqlHelper.ExecuteScalar(connectionString, CommandType.Text, "SELECT  age FROM Test   WHERE id=2");
+            //            int age = Convert.ToInt32(obj) + 1;
+            //            int num1 = MySqlHelper.ExecuteNonQuery(tran, CommandType.Text, "INSERT INTO `ourtool`.`Test` ( `Name`, `CreateTime`, `Age`) VALUES ('梁朝伟', NOW(), 11) ;");
+            //            int num2 = MySqlHelper.ExecuteNonQuery(connectionString, CommandType.Text, "UPDATE  Test  SET  age=" + age + " WHERE id=2");
+            //
+            //            int num3 = MySqlHelper.ExecuteNonQuery(tran, CommandType.Text, "INSERT INTO `ourtool`.`Test` ( `Name`, `CreateTime`, `Age`) VALUES ('周润发', NOW(), 11) ;");
+            //
+            //
+            //            try
+            //            {
+            //                tran.Commit();
+            //                conn.Dispose();
+            //
+            //
+            //            }
+            //            catch (Exception ex)
+            //            {
+            //                tran.Rollback();
+            //                string msg = ex.Message;
+            //
+            //            } 
+            #endregion
 
         }
+
+
 
 
 
